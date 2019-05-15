e1becee1410daec8917c73c7677398b2c5f990c9
dhagandev <dhagan@utexas.edu>
2019-05-14 22:01:38 -0500

    Express Lab - Skills

---
+++ models/skill.js
@@ -0,0 +1,33 @@
+const skills = [
+  {skill: 'JavaScript'},
+  {skill: 'HTML'},
+  {skill: 'CSS'}
+];
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  deleteOne, 
+  update
+};
+
+function update(id, skill) {
+	skills[id] = skill;
+}
+
+function deleteOne(id) {
+	skills.splice(id, 1);
+}
+
+function create(skill) {
+	skills.push(skill);
+}
+
+function getOne(id) {
+  return skills[id];
+}
+
+function getAll() {
+  return skills;
+}
